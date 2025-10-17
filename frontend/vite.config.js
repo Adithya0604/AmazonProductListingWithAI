8e1978880f293a40873fd5bb96eb6f9b24b0c83b
S.Adithya Sharma <120710617+Adithya0604@users.noreply.github.com>
2025-10-17 12:31:39 +0530

    last

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,11 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/AmazonProductListingWithAI/", // <-- important for GitHub Pages
+  base: "/AmazonProductListingWithAI/",  
   plugins: [react()],
   server: {
     proxy: {
-      "/api": "http://localhost:9003", // backend server URL (works only locally)
+      "/api": "http://localhost:9003",
     },
   },
 });
