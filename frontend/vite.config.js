3247b7e08f6a54a3a7bc3e48a704fd24c0d58b55
S.Adithya Sharma <120710617+Adithya0604@users.noreply.github.com>
2025-10-17 12:28:51 +0530

    Gp-Pages

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,10 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/AmazonProductListingWithAI/", // <-- important for GitHub Pages
   plugins: [react()],
   server: {
     proxy: {
-      "/api": "http://localhost:9003", // backend server URL
+      "/api": "http://localhost:9003", // backend server URL (works only locally)
     },
   },
 });
